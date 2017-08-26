2ffda2f2fc382c0aaef338e92d74582528cb15b6
Kate Mashokha <katyamashokha@yahoo.com>
2017-08-26 10:53:29 -0700

    Add paths for a tournament and define the ones to get implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,13 @@ import com.google.gson.Gson;
 import dao.Sql2oGrandSlamDao;
 import dao.Sql2oTennisPlayerDao;
 import dao.TennisPlayerDao;
+import models.GrandSlam;
 import models.TennisPlayer;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class Main {
@@ -17,12 +21,12 @@ public class Main {
         Connection connection = sql2o.open();
 
         // filters
-        after((req, res) ->{
+        after((req, res) -> {
             res.type("application/json");
         });
 
         // TENNIS PLAYERS
-        // create
+        // CREATE
         post("/players/new", "application/json", (request, response) -> {
             TennisPlayer player = gson.fromJson(request.body(), TennisPlayer.class);
             tennisPlayerDao.add(player);
@@ -30,13 +34,35 @@ public class Main {
             return gson.toJson(player);
         });
 
-        // read
+        //TODO
+        // add a country to a players
+
+        //TODO
+        // add a player to tournament
+
+        // READ
+        // all players
         get("/players", "application/json", (request, response) -> {
-            tennisPlayerDao.getAllPlayers();
-            return gson.toJson(tennisPlayerDao.getAllPlayers().size());
+            List<TennisPlayer> players = tennisPlayerDao.getAllPlayers();
+            return gson.toJson(players);
         });
 
-        // update
+        // single player
+        // TODO: handle an exception
+        get("/players/:id", "application/json", (request, response) -> {
+            int playerId = Integer.parseInt(request.params("id"));
+            TennisPlayer player = tennisPlayerDao.findById(playerId);
+            return gson.toJson(player);
+        });
+
+        //TODO
+        // get all tournaments won by a player
+
+
+        // TODO
+        // get all players for a country
+
+        // UPDATE
         post("/players/:id/update", "applciation/json", (request, response) -> {
             int playerId = Integer.parseInt(request.params("id"));
             TennisPlayer player = gson.fromJson(request.body(), TennisPlayer.class);
@@ -45,17 +71,63 @@ public class Main {
             return gson.toJson(player);
         });
 
-        // delete
+        // DELETE
+        // single player
         get("/players/:id/delete", "application/json", (request, response) -> {
             int playerId = Integer.parseInt(request.params("id"));
             tennisPlayerDao.deletePlayer(playerId);
             return gson.toJson(playerId);
         });
 
+        // all players
         get("/players/delete", "application/json", (request, response) -> {
             tennisPlayerDao.deleteAllPlayers();
             return gson.toJson(tennisPlayerDao.getAllPlayers().size());
         });
 
+
+        // TOURNAMENTS
+        //CREATE
+        post("/tournaments/new", "application/json", (request, response) -> {
+            GrandSlam tournament = gson.fromJson(request.body(), GrandSlam.class);
+            grandSlamDao.add(tournament);
+            response.status(201);
+            return gson.toJson(tournament);
+        });
+
+        // TODO:
+        // add a tournament to a player
+
+        //READ
+        // all tournaments
+        get("/tournaments", "application/json", (request, response) -> {
+            List<GrandSlam> tournaments = grandSlamDao.getAllTornaments();
+            return gson.toJson(tournaments);
+        });
+
+        // single tournament
+        // TODO: handle an exception
+        get("/tournaments/:tournId", "application/json", (request, response) -> {
+            int tournId = Integer.parseInt(request.params("tournId"));
+            GrandSlam tournament = grandSlamDao.findById(tournId);
+            return gson.toJson(tournament);
+        });
+
+        // TODO:
+        // get all players won the tournament
+
+        //DELETE
+        // single tournament
+        get("/tournaments/:tournId/delete", "application/json", (request, response) -> {
+            int tournId = Integer.parseInt(request.params("tournId"));
+            grandSlamDao.deleteTourn(tournId);
+            return gson.toJson(tournId);
+        });
+
+        // all tournaments
+        get("/tournaments/delete", "application/json", (request, response) -> {
+            grandSlamDao.deleteAllTourn();
+            return gson.toJson(grandSlamDao.getAllTornaments().size());
+        });
     }
 }
